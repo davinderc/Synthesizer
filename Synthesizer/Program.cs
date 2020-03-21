a3dc2ba91fbe0f2cc95d7b436fbfc8b6d5eaefe5
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-03-21 22:17:02 +0100

    Add project files.

---
+++ Synthesizer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Media;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Synthesizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer();
+            int frequency = 440;
+            double duration = 0.5;
+            var sineWave = new SignalGenerator()
+            {
+                Gain = 0.1,
+                Frequency = frequency,
+                Type = SignalGeneratorType.Sin
+            }.Take(TimeSpan.FromSeconds(duration));
+
+            using (var waveOut = new WaveOutEvent())
+            {
+                waveOut.Init(sineWave);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private static void generateWaveform(float frequency)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void playTone(List<float> waveform)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Synthesizer/requirements.md
@@ -0,0 +1,7 @@
+Requirements for the Synthesizer command line program:
+- Keyboard entry
+- JSON configuration of keys' notes
+- Play note for duration of keypress
+- Adjust ADSR
+- Play notes through speakers
+- 
