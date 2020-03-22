1190147682b6207ad3e4bd2686a3e089548b431e
Davinder Chandhok <davinder.chandhok@zuehlke.com>
2020-03-22 20:51:02 +0100

    Add multiple notes, need to add tests and make the repeated if structure more compact.

--- Synthesizer/Program.cs
+++ Synthesizer/Program.cs
@@ -14,29 +14,70 @@ namespace Synthesizer
         [STAThread]
         static void Main(string[] args)
         {
-            // TODO: Read keyboard inputs in non-blocking manner
-            SoundPlayer player = new SoundPlayer();
-            double frequency = 440;
-            double duration = 0.5;
-            int ms_duration = Convert.ToInt32(duration * 1000);
-            bool play_key = false;
-            if (Keyboard.IsKeyDown(Key.A))
+            while (!Keyboard.IsKeyDown(Key.Escape))
             {
-                // play note or set flag to play note
-                Console.WriteLine($"Key pressed: A");
-                play_key = true;
-            }
-            if (play_key)
-            {
-                ISampleProvider sineWave = generateWaveform(frequency, duration);
-                playTone(sineWave, ms_duration);
-                play_key = false;
-            }
-            if (Keyboard.IsKeyDown(Key.Escape))
-            {
-                return;
-            }
-            
+                // TODO: Read keyboard inputs in non-blocking manner
+                SoundPlayer player = new SoundPlayer();
+                double frequency = 440;
+                double duration = 0.5;
+                int ms_duration = Convert.ToInt32(duration * 1000);
+                bool play_key = false;
+                if (Keyboard.IsKeyDown(Key.A))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: A");
+                    frequency = 261.63;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.S))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: S");
+                    frequency = 293.66;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.D))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: D");
+                    frequency = 329.63;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.F))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: F");
+                    frequency = 349.23;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.Y))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: Y");
+                    frequency = 392.00;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.X))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: X");
+                    frequency = 440.00;
+                    play_key = true;
+                }
+                if (Keyboard.IsKeyDown(Key.C))
+                {
+                    // play note or set flag to play note
+                    Console.WriteLine($"Key pressed: C");
+                    frequency = 493.88;
+                    play_key = true;
+                }
+                if (play_key)
+                {
+                    ISampleProvider sineWave = generateWaveform(frequency, duration);
+                    playTone(sineWave, ms_duration);
+                    play_key = false;
+                }
+            }            
         }
 
         private static ISampleProvider generateWaveform(double frequency, double duration)
